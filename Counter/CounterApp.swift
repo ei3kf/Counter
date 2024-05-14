b1595ff2fdf2be2bf20f24afe0582d852e1707eb
AndyC <andy@ancl.org>
2024-05-14 17:43:43 +0100

    Initial Commit

---
+++ Counter/CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterApp.swift
+//  Counter
+//
+//  Created by Andrew Clements on 14/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

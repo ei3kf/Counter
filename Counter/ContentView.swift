131a501f198bafda0065512b7338fe9049a4b1ee
AndyC <andy@ancl.org>
2024-05-14 23:21:08 +0100

    First Commit

--- Counter/ContentView.swift
+++ Counter/ContentView.swift
@@ -8,16 +8,69 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var count: Int = 0
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Counter")
+                .font(.title)
+                .padding()
+            HStack{
+                ZStack {
+                    Circle().fill(Color.yellow)
+                    Button("+") {
+                        self.count += 1
+                    }
+                }.frame(width: 100, height: 100)
+                
+                ZStack() {
+                    Rectangle().fill(Color.clear)
+                    Text("\(count)")
+                }.frame(width: 100, height: 100)
+                
+                ZStack {
+                    Circle().fill(Color.yellow)
+                    Button("-") {
+                        self.count -= 1
+                    }
+                }.frame(width: 100, height: 100)
+                
+            }.padding()
+            
+            HStack {
+                Spacer()
+                
+                ZStack {
+                    Circle().fill(Color.yellow)
+                    Button("+10") {
+                        self.count += 10
+                    }
+                }.frame(width: 100, height: 100)
+                
+                ZStack {
+                    Circle().fill(Color.yellow)
+                    Button("Reset") {
+                        self.count = 0
+                    }
+                }.frame(width: 100, height: 100)
+                
+                ZStack {
+                    Circle().fill(Color.yellow)
+                    Button("-10") {
+                        self.count -= 10
+                    }
+                }.frame(width: 100, height: 100)
+                
+                Spacer()
+            }
+            
+            Text("2024 Andy Clements EI3KF")
+                .padding()
         }
-        .padding()
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
